7062951bcb87286884fed92a2e93c6c992cf813f
Annnhema <anelligrande@gmail.com>
2024-12-16 15:47:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// Función para redirigir al hacer click en el logo
-function goHome() {
-    window.location.href = "#latte"; // Cambiar la URL según sea necesario
-}
+// Redirigir al hacer clic en "Aquí" (Noticias)
+document.getElementById('noticias-button').addEventListener('click', function() {
+    window.location.href = 'noticias.html'; // Cambiar 'noticias.html' al archivo adecuado
+});
+
+// Redirigir al hacer clic en el botón "Ver más" para Noticias Recientes
+document.getElementById('ver-mas-recientes').addEventListener('click', function() {
+    window.location.href = 'recientes.html'; // Cambiar 'recientes.html' al archivo adecuado
+});
+
+// Redirigir al hacer clic en el botón "Ver más" para Historias de Starbucks
+document.getElementById('ver-mas-historias').addEventListener('click', function() {
+    window.location.href = 'historias.html'; // Cambiar 'historias.html' al archivo adecuado
+});
